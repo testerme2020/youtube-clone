9607209dfa3d73115c9754eb1ab54c2600a32add
Muhammed Farzin <muhammedfarzink5@gmail.com>
2022-12-18 20:49:03 +0530

    Label added on bottom sheet Library item

---
+++ lib/presentation/main/screen_main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ScreenMain extends StatelessWidget {
+  const ScreenMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.face_outlined),
+            activeIcon: Icon(Icons.face),
+            label: 'Shorts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            activeIcon: Icon(Icons.add_circle),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.subscriptions_outlined),
+            activeIcon: Icon(Icons.subscriptions),
+            label: 'Subscriptions',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library_outlined),
+            activeIcon: Icon(Icons.video_library),
+            label: 'Library',
+          ),
+        ],
+      ),
+    );
+  }
+}

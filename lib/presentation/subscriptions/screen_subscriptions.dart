546674fb35c093cbe1ad2bb3b807bfcf73631f59
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-01-01 23:27:38 +0530

    Screen Subscription design completed

--- lib/presentation/subscriptions/screen_subscriptions.dart
+++ lib/presentation/subscriptions/screen_subscriptions.dart
@@ -1,12 +1,34 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:youtube/presentation/subscriptions/widgets/subscription_channels_bottom_list.dart';
+import 'package:youtube/presentation/widgets/custom_sliver_app_bar.dart';
+import 'package:youtube/presentation/widgets/video_list_item_widget.dart';
 
 class ScreenSubscriptions extends StatelessWidget {
   const ScreenSubscriptions({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Screen subscriptions"),
+    return Scaffold(
+      body: SafeArea(
+        child: ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: CustomScrollView(
+            slivers: [
+              const CustomSliverAppBar(
+                bottom: SubscriptionChannelsBottomList(),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return const VideoListItemsWidget();
+                  },
+                  childCount: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/presentation/subscriptions/widgets/subscription_channels_bottom_list.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionChannelsBottomList extends StatelessWidget
+    implements PreferredSizeWidget {
+  const SubscriptionChannelsBottomList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    // TODO: Write your code here
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.white12,
+                        ),
+                        SizedBox(
+                          width: 60,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8.0),
+                            child: Center(
+                              child: Text(
+                                "WB Kids",
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.white54,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: 20,
+            ),
+          ),
+
+          // All Button
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                width: 50,
+                child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateColor.resolveWith(
+                        (states) => Colors.white12),
+                  ),
+                  child: const Text(
+                    "ALL",
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(
+        double.infinity,
+        100,
+      );
+}

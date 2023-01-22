18fbe7baca596808dec93504fb70c71692f325ef
Muhammed Farzin <muhammedfarzink5@gmail.com>
2023-01-22 11:54:58 +0530

    ScreenChannel Created

---
+++ lib/presentation/channel/screen_channel.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScreenChannel extends StatelessWidget {
+  const ScreenChannel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
